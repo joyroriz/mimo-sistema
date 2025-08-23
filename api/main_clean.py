d65ee65f64b8d3f539e0168f20ade4c5dff1588a
joyroriz <joywroriz@gmail.com>
2025-08-23 17:05:28 -0300

    CORRECAO FINAL: Ajustar visualizacao venda para usar modelos corretos

--- api/main_clean.py
+++ api/main_clean.py
@@ -631,31 +631,33 @@ def vendas_nova():
 def vendas_detalhes(venda_id):
     """Visualiza detalhes de uma venda específica"""
     try:
-        venda = Venda.query.get_or_404(venda_id)
-        cliente = Cliente.query.get(venda.cliente_id)
-        itens = ItemVenda.query.filter_by(venda_id=venda_id).all()
+        # Buscar venda usando o modelo correto
+        venda = Venda.buscar_por_id(venda_id)
+        if not venda:
+            return render_template('em_desenvolvimento.html',
+                                 modulo=f'Venda #{venda_id}',
+                                 erro='Venda não encontrada')
 
-        # Buscar produtos dos itens
-        produtos_info = {}
-        for item in itens:
-            produto = Produto.query.get(item.produto_id)
-            if produto:
-                produtos_info[item.id] = produto
+        # Buscar cliente
+        cliente = Cliente.buscar_por_id(venda['cliente_id'])
+
+        # Buscar itens da venda
+        itens = ItemVenda.listar_por_venda(venda_id)
 
         # Calcular valores
         valor_total = 0
         for item in itens:
-            item_total = item.quantidade * item.preco_unitario
+            item_total = item['quantidade'] * item['preco_unitario']
             valor_total += item_total
 
         # Aplicar desconto se houver
-        valor_com_desconto = valor_total - (venda.desconto or 0)
+        desconto = venda.get('desconto', 0) or 0
+        valor_com_desconto = valor_total - desconto
 
         return render_template('venda_detalhes.html',
                              venda=venda,
                              cliente=cliente,
                              itens=itens,
-                             produtos_info=produtos_info,
                              valor_total=valor_total,
                              valor_com_desconto=valor_com_desconto)
 
